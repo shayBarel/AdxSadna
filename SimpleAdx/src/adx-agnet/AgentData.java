200bb13032f387a0382468a25c74055253346882
Nir Attar <nattar@trustwave.com>
2016-03-05 13:14:15 +0200

    * agent data is now singleton

--- SimpleAdx/src/adx-agnet/AgentData.java
+++ SimpleAdx/src/adx-agnet/AgentData.java
@@ -1,24 +1,50 @@
 
 /** 
  * a "global" class that contains all data about our agent .
- * may be replaced by a singleton.
+ * implements a singleton behavior.
  * @author NAttar
  *
  */
 public class AgentData 
 {
 
-	static double _QualityRating = 1.0;
+	//Implementation of singleton.
+	private static AgentData _instance = null ;
+
+	protected AgentData() {
+		//prevent instantiation
+	}
 	
+	public static AgentData GetInstance() 
+	{
+		if(_instance == null) 
+		{
+			_instance = new AgentData();
+		}
+		return _instance;
+	}
 	
-	public static double GetQualityRating () 
+	//end Implementation of singleton.
+	///////////////////////////////
+	
+	//////////////////////////////
+	/// agent data & capabilities.
+	//////////////////////////////
+
+	
+	double _QualityRating = 1.0;
+	
+	public double GetQualityRating () 
 	{
 		return _QualityRating;
 	}
 	
-	public static void SetQualityRating (double new_qr) 
+	public void SetQualityRating (double new_qr) 
 	{
 		_QualityRating = new_qr;
 	}
+	
+	
+
 	
 }
