ee924aac1c77f4b7d06fbb0cf965c2d79219bf41
shayBarel <sbarelozana@gmail.com>
2016-04-12 16:40:15 +0300

    added new class

---
+++ SimpleAdx/src/adx-agnet/UcsDailyReport.java
@@ -0,0 +1,56 @@
+
+public class UcsDailyReport {
+	
+	public double level;
+	public double price;
+	public double target;
+	public double bid;
+	
+	public UcsDailyReport(double level,double price,double target,double bid){
+		this.level = level;
+		this.price = price;
+		this.target = target;
+		this.bid = bid;
+	}
+
+	public double getLevel() {
+		return level;
+	}
+
+	public void setLevel(double level) {
+		this.level = level;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getTarget() {
+		return target;
+	}
+
+	public void setTarget(double target) {
+		this.target = target;
+	}
+
+	public double getBid() {
+		return bid;
+	}
+
+	public void setBid(double bid) {
+		this.bid = bid;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
